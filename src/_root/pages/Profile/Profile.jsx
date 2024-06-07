b7d473dea19da931e7c20033f2cac4ce390d8305
Bianchi-marina <141640079+Bianchi-marina@users.noreply.github.com>
2024-06-07 16:33:13 -0300

    profile page adicionado

--- src/_root/pages/Profile/Profile.jsx
+++ src/_root/pages/Profile/Profile.jsx
@@ -1,10 +1,24 @@
-
+import './Profile.css';
+import GridPost from '../../../components/GridPost/GridPost';
 
 const Profile = () => {
   return (
-    <div>
-      Profile!!
-    </div>
+    <section className="profile-container">
+      <div className="profile-content">
+        <div className="intro-text">
+          <h2>Seus Checks</h2>
+          <p>Aqui você pode visualizar, editar e excluir seus posts</p>
+        </div>
+        {/* <div className="user-posts">
+          {userPosts.length === 0 ? (
+            <p>Você ainda não fez nenhum check-in.</p>
+          ) : (
+            <GridPost posts={userPosts} />
+          )}
+        </div> */}
+        <GridPost />
+      </div>
+    </section>
   )
 }
 
