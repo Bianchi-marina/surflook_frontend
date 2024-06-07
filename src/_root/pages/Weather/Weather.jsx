93937a70b2697f57b0eac1e6e1bbba9ddb3dd8e6
Bianchi-marina <141640079+Bianchi-marina@users.noreply.github.com>
2024-06-07 15:45:19 -0300

    weather input

--- src/_root/pages/Weather/Weather.jsx
+++ src/_root/pages/Weather/Weather.jsx
@@ -1,10 +1,26 @@
+import "./Weather.css";
+import search from "../../../assets/dark/search.png";
+import WeatherGrid from "../../../components/WeatherGrid/WeatherGrid";
 
 const Weather = () => {
   return (
-    <div>
-      Weather!!!
-    </div>
-  )
-}
+    <section className="weather-container">
+      <div className="search-weather-content">
+        <div className="intro-text">
+          <p>Procure checks por cidade e praias</p>
+        </div>
+        <div className="search-input">
+          <input type="text" placeholder="Digite a cidade e praia..." />
+          <button>
+            <img src={search} alt="Search" />
+          </button>
+        </div>
+      </div>
+      <div>
+        <WeatherGrid />
+      </div>
+    </section>
+  );
+};
 
-export default Weather
+export default Weather;
---
+++ src/components/WeatherGrid/WeatherGrid.jsx
@@ -0,0 +1,11 @@
+import './WeatherGrid.css'
+
+const WeatherGrid = () => {
+  return (
+    <div>
+      FAZER O GRID
+    </div>
+  )
+}
+
+export default WeatherGrid
