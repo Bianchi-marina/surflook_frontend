9487a378a48fe070e154c249fbc5885ecad28812
Bianchi-marina <141640079+Bianchi-marina@users.noreply.github.com>
2024-06-07 15:31:48 -0300

    search input adicionado

--- src/_root/pages/Location/Location.jsx
+++ src/_root/pages/Location/Location.jsx
@@ -1,11 +1,19 @@
-import GridPost from "../../../components/GridPost/GridPost"
 import './Location.css'
+import search from "../../../assets/dark/search.png"
 
 const Location = () => {
   return (
-    <div>
-      <GridPost />
+    <section className="search-by-location-container">
+    <div className="search-by-location-content">
+      <div className="intro-text">
+        <p>Procure checks por cidade e praias</p>
+      </div>
+      <div className="search-input">
+        <input type="text" placeholder="Digite a cidade e praia..." />
+        <button><img src={search}/></button>
+      </div>
     </div>
+  </section>
   )
 }
 
