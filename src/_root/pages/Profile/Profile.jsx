300798b895c92e392415eb03764aac523c05d570
Bianchi-marina <141640079+Bianchi-marina@users.noreply.github.com>
2024-06-08 10:03:22 -0300

    page profile com user-infos para editar ali na pagina

--- src/_root/pages/Profile/Profile.jsx
+++ src/_root/pages/Profile/Profile.jsx
@@ -1,25 +1,31 @@
-import './Profile.css';
-import GridPost from '../../../components/GridPost/GridPost';
+import "./Profile.css";
+import GridPost from "../../../components/GridPost/GridPost";
+import user from "../../../assets/light/profile.png";
+import edit from "../../../assets/light/edit.png"
 
 const Profile = () => {
   return (
     <section className="profile-container">
       <div className="profile-content">
+        <div className="profile-infos">
+          <div className="user-container">
+            <img src={user} alt="User Avatar" className="user-avatar" />
+            <div className="user-info">
+              <p className="user-title">Aloha,</p>
+              <h2>Username</h2>
+            </div>
+          </div>
+          <button className="edit-profile-button"><img src={edit} alt="" className=""/></button>
+        </div>
+
         <div className="intro-text">
           <h2>Seus Checks</h2>
-          <p>Aqui você pode visualizar, editar e excluir seus posts</p>
+          <p>Aqui você pode visualizar, editar e excluir seus checks</p>
         </div>
-        {/* <div className="user-posts">
-          {userPosts.length === 0 ? (
-            <p>Você ainda não fez nenhum check-in.</p>
-          ) : (
-            <GridPost posts={userPosts} />
-          )}
-        </div> */}
         <GridPost />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
