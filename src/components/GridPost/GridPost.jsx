9519a8299defaa3e94f7ebc739b45bb64b74a5ad
Bianchi-marina <141640079+Bianchi-marina@users.noreply.github.com>
2024-06-08 20:13:21 -0300

    Icons posts adicionados

--- src/components/GridPost/GridPost.jsx
+++ src/components/GridPost/GridPost.jsx
@@ -1,18 +1,37 @@
-import './GridPost.css';
+import "./GridPost.css";
+import fotoIcon from "../../assets/light/image.png"; // Certifique-se de usar o caminho correto para os ícones
+import trashIcon from "../../assets/light/trash.png";
+import likeIcon from "../../assets/light/like.svg";
 
-const GridPost = () => {
+const GridPost = ({ onConfirm }) => {
   return (
     <div className="grid-post">
       <div className="post">
-        <img src="image-url" alt="Post" className="post-image" />
+        <div className="post-media">
+          <img src="" alt="Post" className="post-image" />
+          <div className="post-icons">
+            <span className="icon">
+              <img src={fotoIcon} alt="Foto Icon" />
+            </span>
+            <button className="post-icon" onClick={onConfirm}>
+              <img src={trashIcon} alt="Trash Icon" />
+            </button>
+           
+          </div>
+          <div className="like-container">
+              <button className="post-icon">
+                <img src={likeIcon} alt="Like Icon" />
+              </button>
+            </div>
+        </div>
         <div className="post-content">
-          <p className="username">Username</p>
+          <p className="username">Marina</p>
           <div className="tags">
             <span className="tag">Cidade</span>
             <span className="tag">Praia</span>
             <span className="tag">Hora</span>
           </div>
-          <p className="description">Descrição do post</p>
+          <p className="description">Altas hoje!</p>
         </div>
       </div>
     </div>
